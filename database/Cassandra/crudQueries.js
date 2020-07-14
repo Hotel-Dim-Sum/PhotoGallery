7d4f5a6e4c1d1c93489592622e21ab9cb979c1e2
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-07-14 16:44:31 -0700

    Finalizes Potential Cassandra Queries
    Adds the finalized versions of the Cassandra queries that would be used
    in the API's CRUD operations. All are currently running under sub 50ms
    and some are under 10.

--- database/Cassandra/crudQueries.js
+++ database/Cassandra/crudQueries.js
@@ -1,20 +1,28 @@
 const client = require('./index.js');
 
-function getRoomData(roomId, callback) {
-  "SELECT * FROM photos_by_room WHERE city = cityName AND image_id >= firstId AND image_id <= lastId"
+function getRoomData(cityName, firstId, lastId, callback) {
+  let qString = 'SELECT * FROM photos_by_room WHERE city = ? AND image_id >= ? AND image_id <= ?';
+  let variables = [cityName, firstId, lastId];
+  client.execute(qString, variables, callback);
 }
 
-function createNewPhoto(listName, isSaved, roomId, userId, callback) {
-  "INSERT INTO photos_by_room (room_id, user_id, room_name, city, state, image_id, image_url, image_description, list_id, list_name, saved)
-  VALUES (500002, 500002, 'Eum qui aut voluptas necessitatibus.', 'Darienhaven', 'MS', 3501323, 'https://hrsf128hotelphotos.s3-us-west-2.amazonaws.com/image1000.jpg', 'Placeat voluptatem natus', null, null, null)"
+function createNewPhoto(roomId, userId, roomName, cityName, stateAbbr, imageId, imageUrl, imageDesc, listId, listName, isSaved, callback) {
+  let qString = `INSERT INTO photos_by_room (room_id, user_id, room_name, city, state, image_id, image_url, image_description, list_id, list_name, saved)
+  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  let variables = [roomId, userId, roomName, cityName, stateAbbr, imageId, imageUrl, imageDesc, listId, listName, isSaved];
+  client.execute(qString, variables, callback);
 }
 
-function changePhoto(isSaved, listId, roomId, callback) {
-  "UPDATE photos_by_room SET image_url = imageUrl WHERE city = cityName AND image_id = imageId IF EXISTS"
+function changePhoto(imageUrl, cityName, imageId, callback) {
+  let qString = 'UPDATE photos_by_room SET image_url = ? WHERE city = ? AND image_id = ? IF EXISTS';
+  let variables = [imageUrl, cityName, imageId];
+  client.execute(qString, variables, callback);
 }
 
-function deletePhoto(listId, callback) {
-  "DELETE FROM photos_by_room WHERE city = cityName AND image_id = imageId IF EXISTS"
+function deletePhoto(cityName, imageId, callback) {
+  let qString = 'DELETE FROM photos_by_room WHERE city = ? AND image_id = ? IF EXISTS';
+  let variables = [cityName, imageId];
+  client.execute(qString, variables, callback);
 }
 
-module.exports = { getRoomData, createList, changeSaveStatus, deleteList };
+module.exports = { getRoomData, createNewPhoto, changePhoto, deletePhoto };
