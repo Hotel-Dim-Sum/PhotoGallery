b56800ec02059046f0cce957ee1af55515f52e49
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-07-16 22:18:57 -0700

    Adds Cluster Package to Application
    - Adds Cluster NPM package to Application
    - Refactors Server index.js file to make use of the computer's multiple
    cores and create a Node cluster so that I can handle more K6 requests

--- server/index.js
+++ server/index.js
@@ -4,19 +4,28 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 const Controllers = require('./Controllers.js');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 const app = express();
 const port = 3004;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/photogallery', express.static(path.join(__dirname, '../client/dist')));
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+      cluster.fork();
+  }
+} else {
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use('/photogallery', express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/api/:roomId/photo-gallery', Controllers.getRoomData);
+  app.get('/api/:roomId/photo-gallery', Controllers.getRoomData);
 
-app.post('/api/:roomId/photo-gallery/save-status', Controllers.createList);
+  app.post('/api/:roomId/photo-gallery/save-status', Controllers.createList);
 
-app.put('/api/:roomId/photo-gallery/save-status', Controllers.changeSaveStatus);
+  app.put('/api/:roomId/photo-gallery/save-status', Controllers.changeSaveStatus);
 
-app.delete('/api/:roomId/photo-gallery', Controllers.deleteList);
+  app.delete('/api/:roomId/photo-gallery', Controllers.deleteList);
 
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+  app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+}
+
